2ba8c8ad48fda33ccb2887c3ab38025f299fb465
personal <primastech101@gmail.com>
2026-09-03 11:44:51 +0100

    update typeorm migration commands and database source

---
+++ src/database/data-source.ts
@@ -0,0 +1,33 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+import { User } from '../users/entities/user.entity';
+
+config();
+
+const host =
+  process.env.MIGRATION_DB_HOST ||
+  (process.env.DATABASE_HOST === 'postgres'
+    ? 'localhost'
+    : process.env.DATABASE_HOST);
+
+const port =
+  Number(process.env.MIGRATION_DB_PORT) ||
+  (process.env.DATABASE_HOST === 'postgres'
+    ? 5433
+    : Number(process.env.DATABASE_PORT));
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host,
+  port,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [User],
+  migrations: ['src/database/migrations/*.ts'],
+  synchronize: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
